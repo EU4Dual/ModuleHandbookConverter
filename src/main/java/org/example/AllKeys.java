7135928e6a3d6a3ad12a279ac13f174b88968150
Kathy Chan <kathy.chan@praesidium.dhbw.de>
2024-02-26 15:41:58 +0100

    added class

---
+++ src/main/java/org/example/AllKeys.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class AllKeys {
+
+    public String[] allKeys = {
+            "MODULNUMMER",
+            "VERORTUNG IM STUDIENVERLAUF",
+            "MODULDAUER (SEMESTER)",
+            "MODULVERANTWORTUNG",
+            "SPRACHE",
+
+            "LEHRFORMEN",
+            "LEHRMETHODEN",
+
+            "PRÜFUNGSLEISTUNG",
+            "PRÜFUNGSUMFANG (IN MINUTEN)",
+            "BENOTUNG",
+
+            "WORKLOAD INSGESAMT (IN H)",
+            "DAVON PRÄSENZZEIT (IN H)",
+            "DAVON SELBSTSTUDIUM (IN H)",
+            "ECTS-LEISTUNGSPUNKTE",
+
+            "FACHKOMPETENZ",
+            "METHODENKOMPETENZ",
+            "PERSONALE UND SOZIALE KOMPETENZ",
+            "ÜBERGREIFENDE HANDLUNGSKOMPETENZ",
+
+            "LEHR- UND LERNEINHEITEN",
+            "PRÄSENZZEIT",
+            "SELBSTSTUDIUM",
+
+            "BESONDERHEITEN",
+
+            "VORAUSSETZUNGEN",
+
+            "LITERATUR"
+    };
+
+    public String[] sectionHeader = {
+            "FORMALE ANGABEN ZUM MODUL",
+            "EINGESETZTE LEHRFORMEN",
+            "EINGESETZTE PRÜFUNGSFORMEN",
+            "WORKLOAD UND ECTS-LEISTUNGSPUNKTE",
+            "QUALIFIKATIONSZIELE UND KOMPETENZEN",
+            "LERNEINHEITEN UND INHALTE",
+            "BESONDERHEITEN",
+            "VORAUSSETZUNGEN",
+            "LITERATUR"
+    };
+}
